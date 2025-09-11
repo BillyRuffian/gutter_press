ee58317adb546f3d6e276b008c88712a086af6c3
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2025-09-11 14:36:36 +0100

    updated system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |driver_options|
+    driver_options.add_argument('--headless')
+    driver_options.add_argument('--no-sandbox')
+    driver_options.add_argument('--disable-dev-shm-usage')
+    driver_options.add_argument('--disable-gpu')
+    driver_options.add_argument('--disable-web-security')
+    driver_options.add_argument('--disable-extensions')
+    driver_options.add_argument('--window-size=1400,1400')
+    driver_options.add_argument("--user-data-dir=#{Dir.tmpdir}/chrome_test_#{Process.pid}_#{SecureRandom.hex(8)}")
+  end
 end
--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -3,43 +3,53 @@ require 'application_system_test_case'
 class PostsTest < ApplicationSystemTestCase
   setup do
     @post = posts(:one)
+    @user = users(:one)
+    # Ensure the post is published for system tests
+    @post.update!(published_at: 1.hour.ago)
+  end
+
+  private
+
+  def sign_in_as(user)
+    visit new_session_url
+    fill_in 'Email Address', with: user.email_address
+    fill_in 'Password', with: 'password'
+    click_button 'Sign In'
+    # Wait for redirect to complete
+    assert_selector 'h1', text: 'Latest Posts'
   end
 
   test 'visiting the index' do
     visit posts_url
+    assert_selector 'h1', text: 'Latest Posts'
+    assert_selector 'article', minimum: 1
+  end
+
+  test 'should show individual post' do
+    visit post_url(@post)
+    assert_selector 'h1', text: @post.title
+    assert_selector '.post-content'
+    assert_selector '.post-meta'
+  end
+
+  test 'should sign in and access manage interface' do
+    sign_in_as(@user)
+    
+    # Now navigate to manage interface
+    visit manage_root_url
+    assert_selector 'h1', text: 'Dashboard'
+    assert_selector '.card', minimum: 1
+  end
+
+  test 'should manage posts when authenticated' do
+    sign_in_as(@user)
+    
+    # Visit manage posts
+    visit manage_posts_url
     assert_selector 'h1', text: 'Posts'
-  end
-
-  test 'should create post' do
-    visit posts_url
-    click_on 'New post'
-
-    fill_in 'Published at', with: @post.published_at
-    fill_in 'Title', with: @post.title
-    fill_in 'User', with: @post.user_id
-    click_on 'Create Post'
-
-    assert_text 'Post was successfully created'
-    click_on 'Back'
-  end
-
-  test 'should update Post' do
-    visit post_url(@post)
-    click_on 'Edit this post', match: :first
-
-    fill_in 'Published at', with: @post.published_at.to_s
-    fill_in 'Title', with: @post.title
-    fill_in 'User', with: @post.user_id
-    click_on 'Update Post'
-
-    assert_text 'Post was successfully updated'
-    click_on 'Back'
-  end
-
-  test 'should destroy Post' do
-    visit post_url(@post)
-    click_on 'Destroy this post', match: :first
-
-    assert_text 'Post was successfully destroyed'
+    assert_selector 'table'
+    
+    # Check that posts are listed
+    assert_selector 'td', text: @post.title
   end
 end
