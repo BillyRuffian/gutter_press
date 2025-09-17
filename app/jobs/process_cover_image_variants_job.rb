3994849c5d34a758838764b63ff3e70193d9fdab
Nigel Brookes-Thomas <nigel@brookes-thomas.co.uk>
2025-09-17 22:16:49 +0100

    interruptable steps

--- app/jobs/process_cover_image_variants_job.rb
+++ app/jobs/process_cover_image_variants_job.rb
@@ -8,12 +8,16 @@ class ProcessCoverImageVariantsJob < ApplicationJob
     Rails.logger.info "Processing cover image variants for #{postable.class.name}##{postable.id}"
 
     # Generate the thumbnail variant (300x200)
-    thumbnail_variant = postable.cover_image.variant(resize_to_fill: [ 300, 200 ])
-    thumbnail_variant.processed
+    step :thumbnail do
+      thumbnail_variant = postable.cover_image.variant(resize_to_fill: [ 300, 200 ])
+      thumbnail_variant.processed
+    end
 
     # Generate the hero variant (1920x1080 max)
-    hero_variant = postable.cover_image.variant(resize_to_limit: [ 1920, 1080 ])
-    hero_variant.processed
+    step :hero do
+      hero_variant = postable.cover_image.variant(resize_to_limit: [ 1920, 1080 ])
+      hero_variant.processed
+    end
 
     Rails.logger.info "Completed processing cover image variants for #{postable.class.name}##{postable.id}"
 
